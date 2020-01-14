946a0c8277e2141a7793f8014fbc58dbdcb43c6a
unknown <Dustin.Guillen14@yahoo.com>
2020-01-14 11:46:33 -0700

    added 5 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
-// Your code goes here
+// ================ mouseover ================
+let topImg = document.querySelector('.intro img');
+topImg.addEventListener('mouseover', () => {
+    topImg.src = 'https://dreamlimousinesdetroit.com/wp-content/uploads/2016/12/party-buses.jpg';
+});
+topImg.addEventListener('mouseleave', () => {
+    topImg.src = 'img/fun-bus.jpg';
+});
+
+// ================ keydown ================
+document.addEventListener('keydown', () => {
+    alert('Keeps you paws off me you damn dirty apes.')
+});
+
+// ================ wheel ================
+document.addEventListener('wheel', () => {
+    alert('Nice wheels')
+})
+
+// ================ drag / drop ================
+
+
+// ================ load ================
+window.addEventListener('load', (event) => {
+    alert('The page is fully loaded');
+  });
+
+  // ================ focus ================
+
+  // ================ resize ================
+  let boatImg = document.querySelector('.content-destination img');
+  console.log(boatImg)
+  window.addEventListener('resize', () => {
+      boatImg.src = 'https://www.bing.com/th/id/OIP.R3-UjxHAS-hbOYAHlTZpiwHaEK?pid=Api&rs=1';
+  })
+
+  // ================ scroll ================
+
+  // ================ select ================
+
+// ================ dblclick ================
+// const navLink = document.querySelectorAll('nav-link');
+// navLink.forEach(element => {
+//     element.addEventListener('dblclick', () => {
+//         navLink.style.color = 'green';
+//     })
+// })
